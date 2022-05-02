fc86b4d628e75f9c7741e66403c9edbb28c9c3d9
Piotr Gruszewski <piotr.gruszewski@gmail.com>
2022-05-02 10:19:54 +0200

    2.03

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -97,6 +97,9 @@ int main(void)
   {
 
 	 HAL_GPIO_WritePin(LD2_GPIO_Port,LD2_Pin, GPIO_PIN_SET);
+	 HAL_Delay(1000);
+	 HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+	 HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
